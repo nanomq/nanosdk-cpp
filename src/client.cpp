20c70ab88b6f8494049c9b5f632047969b4b12da
lee-emqx <lihj@emqx.io>
2023-03-29 11:21:56 +0800

    * MDF [client.*] move implemention from header file to source file.

--- src/client.cpp
+++ src/client.cpp
@@ -1,5 +1,90 @@
-#include <thread>
-#include <cstdarg>
-#include <cstring>
-
 #include "client.h"
+#include <iostream>
+using namespace std;
+
+
+QuicClient::QuicClient(const char *url)
+{
+
+    int rv;
+    if ((rv = nng_mqtt_quic_client_open(&this->sock, url)) != 0)
+    {
+        cerr << "Error in Quic client open. Return code: " << rv << endl;
+    }
+}
+
+QuicClient::~QuicClient()
+{
+    nng_close(this->sock);
+}
+
+int QuicClient::set_callbacks(Callbacks *cbs)
+{
+    this->cbs = std::move(cbs);
+    if (0 != nng_mqtt_quic_set_connect_cb(&this->sock, on_connected, (void *)this) || 0 != nng_mqtt_quic_set_disconnect_cb(&this->sock, on_disconnected, (void *)this) || 0 != nng_mqtt_quic_set_msg_recv_cb(&this->sock, on_message_received, (void *)this) || 0 != nng_mqtt_quic_set_msg_send_cb(&this->sock, on_message_send, (void *)this))
+    {
+        cerr << "Quic client cb set error.\n";
+        return -1;
+    }
+    return 0;
+}
+
+int QuicClient::connect(uint16_t keepalive, bool clean_session)
+{
+    nng_msg *msg;
+    int rc = 0;
+    nng_mqtt_msg_alloc(&msg, 0);
+    nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_CONNECT);
+    nng_mqtt_msg_set_connect_keep_alive(msg, keepalive);
+    nng_mqtt_msg_set_connect_clean_session(msg, clean_session);
+
+    if ((rc = nng_sendmsg(this->sock, msg, NNG_FLAG_ALLOC) != 0))
+    {
+        cerr << "MQTT_NNG publish error: " << nng_strerror(rc) << endl;
+    }
+    return rc;
+}
+
+int QuicClient::subscribe(char *topic, uint8_t QoS)
+{
+    nng_msg *msg;
+    int rc = 0;
+    nng_mqtt_msg_alloc(&msg, 0);
+    nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_SUBSCRIBE);
+
+    int count = 1;
+
+    nng_mqtt_topic_qos subscriptions[] = {
+        {.topic = {
+             .length = (uint32_t)strlen(topic),
+             .buf = (uint8_t *)topic},
+         .qos = QoS}};
+
+    nng_mqtt_msg_set_subscribe_topics(msg, subscriptions, count);
+    rc = nng_sendmsg(this->sock, msg, NNG_FLAG_ALLOC);
+    if (rc != 0)
+    {
+        cerr << "MQTT_NNG subscribe error: " << nng_strerror(rc) << endl;
+    }
+    return rc;
+}
+
+int QuicClient::publish(char *topic, char *payload, uint8_t QoS, bool dup, bool retain)
+{
+    nng_msg *msg;
+    int rc = 0;
+    nng_mqtt_msg_alloc(&msg, 0);
+    nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_PUBLISH);
+    nng_mqtt_msg_set_publish_dup(msg, dup);
+    nng_mqtt_msg_set_publish_qos(msg, QoS);
+    nng_mqtt_msg_set_publish_retain(msg, retain);
+    nng_mqtt_msg_set_publish_topic(msg, topic);
+    nng_mqtt_msg_set_publish_payload(
+        msg, (uint8_t *)payload, strlen(payload));
+    rc = nng_sendmsg(this->sock, msg, NNG_FLAG_ALLOC);
+    if (rc != 0)
+    {
+        cerr << "MQTT_NNG publish error: " << nng_strerror(rc) << endl;
+    }
+    return rc;
+}
